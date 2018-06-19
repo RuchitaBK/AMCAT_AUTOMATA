38e90ffe817240cb627a6045d74937a4d1d9c970
RuchitaBK <kshirsagarruchita@gmail.com>
2018-06-19 21:23:34 +0530

    with Extra details in code

--- trapezium_pattern.c
+++ trapezium_pattern.c
@@ -37,28 +37,29 @@ int m;
     {
         if(i<n)
         {
-            for(space=1;space<=l;space++)
+            for(space=1;space<=l;space++)              //print inital -- 
             {
                 printf("-");
             }
             l=l+2;
         }
         
-        for(j=1;j<=i;j++)        
-        {
+        for(j=1;j<=i;j++)                      //print part 1*2*3*4
+        {                              // print half part       5*6*7
            
             printf("%d",half_part);
            half_part++;
            printf("*");
             
         }
-          
-          
-          total_no=total_no-i;
+
+          total_no=total_no-i;               // subtract i from total i.e 21-4=17 so next part start from 17 
           second_part=total_no;
-          for(m=1;m<=i;m++)
-          {
-              printf("%d",second_part);
+		  
+		  
+          for(m=1;m<=i;m++)                     //print part 17*18*19*20
+          {                               // print half part 14*15*16
+              printf("%d",second_part);           
               second_part++;
               if(m!=i)
               printf("*");
